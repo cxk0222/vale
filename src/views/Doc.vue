59b72e52cfefdb1291764e6cb5cafa44126cb1cc
Yuriuh <642192014@qq.com>
2020-09-01 16:14:51 +0800

    封装完成 Topnav 组件

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
-    </div>
+    <Topnav/>
     <div class="content">
       <aside>边栏</aside>
       <main>主内容</main>
@@ -11,9 +8,12 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import Topnav from '../components/Topnav.vue'
 export default {
-
+  components: {
+    Topnav
+  }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
-    </div>
+    <Topnav />
     <div class="banner">
       <h1>Vale UI</h1>
       <h2>一个轻量的 UI 框架</h2>
@@ -15,9 +12,12 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import Topnav from '../components/Topnav.vue'
 export default {
-
+  components: {
+    Topnav
+  }
 }
 </script>
 
