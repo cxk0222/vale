6037fcd24d2409be04bd7393cd91f085833a8bb3
Yuriuh <642192014@qq.com>
2020-09-01 17:01:43 +0800

    让 topnav 是响应式并添加 toggleAside

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,10 +1,11 @@
 <template>
 <div class="topnav">
-  <div class="logo" @click="toggleMenu">Logo</div>
+  <div class="logo">Logo</div>
   <ul class="menu">
     <li>菜单1</li>
     <li>菜单2</li>
   </ul>
+  <span class="toggleAside" @click="toggleMenu"></span>
 </div>
 </template>
 
@@ -34,8 +35,10 @@ export default {
   background: pink;
   display: flex;
   padding: 16px;
-  position: absolute;
+  position: relative;
   z-index: 10;
+  justify-content: center;
+  align-items: center;
 
   >.logo {
     max-width: 6em;
@@ -49,6 +52,32 @@ export default {
 
     >li {
       margin: 0 1em;
+    }
+  }
+
+  >.toggleAside {
+    display: inline-block;
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+    display: none;
+  }
+
+  @media (max-width: 500px) {
+    >.menu {
+      display: none;
+    }
+
+    >.logo {
+      margin: 0 auto;
+    }
+
+    >.toggleAside {
+      display: inline-block;
     }
   }
 }
