d130d1e9dbcb0dfa2afb79d6f2160b04dcb5bb7b
Yuriuh <642192014@qq.com>
2020-09-01 21:38:01 +0800

    完成 Switch 切换状态

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,12 +1,26 @@
 <template>
-<button>
+<button :class="{'checked': checked}" @click="toggle">
   <span></span>
 </button>
 </template>
 
 <script lang="ts">
+import {
+  ref
+} from 'vue'
 export default {
+  setup() {
+    const checked = ref(true)
 
+    const toggle = () => {
+      checked.value = !checked.value
+    }
+
+    return {
+      checked,
+      toggle,
+    }
+  }
 }
 </script>
 
@@ -18,18 +32,27 @@ button {
   height: $h;
   width: $h*2;
   border: none;
-  background: blue;
+  background: gray;
   border-radius: $h/2;
   position: relative;
-}
+  cursor: pointer;
 
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2 / 2;
+  &.checked {
+    background: blue;
+  }
+
+  &.checked>span {
+    left: calc(100% - #{$h2} - 2px);
+  }
+
+  span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+  }
 }
 </style>
