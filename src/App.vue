55a62fcd2a9c6742868f3312da601947f8c46159
Yuriuh <642192014@qq.com>
2020-09-01 17:45:40 +0800

    路由切换之后关闭 aside

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,21 @@ import {
   ref,
   provide
 } from 'vue'
+import {
+  router
+} from './router'
+
 export default {
   name: 'App',
   setup() {
     const width = document.documentElement.clientWidth;
     const menuVisible = ref(width <= 500 ? false : true);
     provide('menuVisible', menuVisible)
+    router.afterEach(() => {
+      if (width <= 500) {
+        menuVisible.value = false
+      }
+    })
   }
 }
 </script>
