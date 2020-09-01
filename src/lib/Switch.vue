52b7169d7d127a46812c706e470872e6f0402362
Yuriuh <642192014@qq.com>
2020-09-01 18:48:22 +0800

    Switch css 1

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,7 +1,7 @@
 <template>
-<div>
-  Switch 组件
-</div>
+<button>
+  <span></span>
+</button>
 </template>
 
 <script lang="ts">
@@ -11,5 +11,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
 
+button {
+  height: $h;
+  width: $h*2;
+  border: none;
+  background: blue;
+  border-radius: $h/2;
+  position: relative;
+}
+
+span {
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  background: white;
+  border-radius: $h2 / 2;
+}
 </style>
