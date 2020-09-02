a686d88781ce28cf7b891a50b613b98cdffbc19e
Yuriuh <642192014@qq.com>
2020-09-02 17:46:55 +0800

    Create Button Component

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,12 +1,17 @@
 <template>
+<div>Button 示例</div>
+<h1>示例</h1>
 <div>
-  Button 示例
+  <Button>你好</Button>
 </div>
 </template>
 
 <script>
+import Button from '../lib/Button.vue'
 export default {
-
+  components: {
+    Button
+  }
 }
 </script>
 
---
+++ src/lib/Button.vue
@@ -0,0 +1,15 @@
+<template>
+<button>
+  <slot />
+</button>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
