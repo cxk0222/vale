71876137d1fa60e578b2848698657417efa2e37b
Yuriuh <642192014@qq.com>
2020-09-02 21:20:45 +0800

    Create dialog component

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,12 +1,16 @@
 <template>
-<div>
-  Dialog 示例
-</div>
+  <div>Dialog 示例</div>
+  <div>
+    <Dialog>
+  </div>
 </template>
 
 <script>
+import Dialog from '../lib/Dialog.vue'
 export default {
-
+  components: {
+    Dialog
+  }
 }
 </script>
 
