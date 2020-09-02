a4d332f6ac20224fbea707ab0298c57f6eafde5f
Yuriuh <642192014@qq.com>
2020-09-02 20:01:50 +0800

    Add loading property

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,69 +1,72 @@
 <template>
-<div>Button 示例</div>
-<h2>theme</h2>
-<div>
+  <div>Button 示例</div>
+  <h2>theme</h2>
   <div>
-    <Button>你好</Button>
+    <div>
+      <Button>你好</Button>
+    </div>
+    <br>
+    <div>
+      <Button theme="button">你好</Button>
+    </div>
+    <br>
+    <div>
+      <Button theme="link">你好</Button>
+    </div>
+    <br>
+    <div>
+      <Button theme="text">你好</Button>
+    </div>
   </div>
-  <br>
+  <h2>size</h2>
   <div>
-    <Button theme="button">你好</Button>
+    <div>
+      <Button size="big">大大大</Button>
+      <Button>中中中</Button>
+      <Button size="small">小小小</Button>
+    </div>
+    <div>
+      <Button theme="link" size="big">大大大</Button>
+      <Button theme="link">中中中</Button>
+      <Button theme="link" size="small">小小小</Button>
+    </div>
+    <div>
+      <Button theme="text" size="big">大大大</Button>
+      <Button theme="text">中中中</Button>
+      <Button theme="text" size="small">小小小</Button>
+    </div>
   </div>
-  <br>
+  <h2>level</h2>
   <div>
-    <Button theme="link">你好</Button>
+    <div>
+      <Button level="main">主要按钮</Button>
+      <Button>普通按钮</Button>
+      <Button level="danger">危险按钮</Button>
+    </div>
+    <div>
+      <Button theme="link" level="main">主要链接按钮</Button>
+      <Button theme="link">普通链接按钮</Button>
+      <Button theme="link" level="danger">危险链接按钮</Button>
+    </div>
+    <div>
+      <Button theme="text" level="main">主要文字按钮</Button>
+      <Button theme="text">普通文字按钮</Button>
+      <Button theme="text" level="danger">危险文字按钮</Button>
+    </div>
   </div>
-  <br>
+  <h2>disabled</h2>
   <div>
-    <Button theme="text">你好</Button>
-  </div>
-</div>
-<h2>size</h2>
-<div>
-  <div>
-    <Button size="big">大大大</Button>
-    <Button>中中中</Button>
-    <Button size="small">小小小</Button>
-  </div>
-  <div>
-    <Button theme="link" size="big">大大大</Button>
-    <Button theme="link">中中中</Button>
-    <Button theme="link" size="small">小小小</Button>
-  </div>
-  <div>
-    <Button theme="text" size="big">大大大</Button>
-    <Button theme="text">中中中</Button>
-    <Button theme="text" size="small">小小小</Button>
-  </div>
-</div>
-<h2>level</h2>
-<div>
-  <div>
-    <Button level="main">主要按钮</Button>
-    <Button>普通按钮</Button>
-    <Button level="danger">危险按钮</Button>
+    <div>
+      <Button disabled>禁用按钮1</Button>
+      <Button theme="link" disabled>禁用链接按钮</Button>
+      <Button theme="text" disabled>禁用文字按钮</Button>
+    </div>
   </div>
   <div>
-    <Button theme="link" level="main">主要链接按钮</Button>
-    <Button theme="link">普通链接按钮</Button>
-    <Button theme="link" level="danger">危险链接按钮</Button>
-  </div>
+  <h2>loading</h2>
   <div>
-    <Button theme="text" level="main">主要文字按钮</Button>
-    <Button theme="text">普通文字按钮</Button>
-    <Button theme="text" level="danger">危险文字按钮</Button>
+    <Button loading>加载中</Button>
   </div>
-</div>
-<h2>disabled</h2>
-<div>
-  <div>
-    <Button disabled>禁用按钮1</Button>
-    <Button theme="link" disabled>禁用链接按钮</Button>
-    <Button theme="text" disabled>禁用文字按钮</Button>
-  </div>
-</div>
-<div>
-
 </div>
 </template>
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,6 @@
 <template>
 <button class="vale-button" :class="classes" :disabled="disabled">
+  <span v-if="loading" class="vale-loading-indicator"></span>
   <slot />
 </button>
 </template>
@@ -23,6 +24,10 @@ export default {
       default: 'normal',
     },
     disabled: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
       type: Boolean,
       default: false,
     }
@@ -198,6 +203,23 @@ $radius: 4px;
       cursor: not-allowed;
       color: $grey;
     }
+  }
+
+  >.vale-loading-indicator {
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: vale-spin 1s infinite linear;
+  }
+
+  @keyframes vale-spin {
+    0% { transform: rotate(0deg) }
+    100% { transform: rotate(360deg) }
   }
 }
 </style>
