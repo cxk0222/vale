322cadcfa2b52161279513fa9499320310f3f390
Yuriuh <642192014@qq.com>
2020-09-21 18:02:28 +0800

    Remove useless code and files

--- src/router.ts
+++ src/router.ts
@@ -8,9 +8,6 @@ import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import DocDemo from './components/DocDemo.vue'
 
-// import Intro from './views/Intro.vue'
-// import GetStarted from './views/GetStarted.vue'
-// import Install from './views/Install.vue'
 import { h } from 'vue'
 import Markdown from './components/Markdown.vue'
 
