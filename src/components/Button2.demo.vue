811d47d91e92b4e6016a61284b21cdf075ffcb53
Yuriuh <642192014@qq.com>
2020-09-21 21:39:00 +0800

    Update Button demos

---
+++ src/components/Button2.demo.vue
@@ -0,0 +1,32 @@
+<demo>
+支持 size
+</demo>
+
+<template>
+  <div>
+    <div>
+      <Button size="big">大大大</Button>
+      <Button>中中中</Button>
+      <Button size="small">小小小</Button>
+    </div>
+    <div>
+      <Button theme="link" size="big">大大大</Button>
+      <Button theme="link">中中中</Button>
+      <Button size="small" theme="link">小小小</Button>
+    </div>
+    <div>
+      <Button size="big" theme="text">大大大</Button>
+      <Button theme="text">中中中</Button>
+      <Button size="small" theme="text">小小小</Button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue"
+export default {
+  components: {
+    Button
+  }
+}
+</script>
---
+++ src/components/Button4.demo.vue
@@ -0,0 +1,20 @@
+<demo>
+支持 disabled
+</demo>
+
+<template>
+  <div>
+    <Button disabled>禁用按钮</Button>
+    <Button theme="link" disabled>禁用链接按钮</Button>
+    <Button theme="text" disabled>禁用按钮</Button>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue"
+export default {
+  components: {
+    Button
+  }
+}
+</script>
