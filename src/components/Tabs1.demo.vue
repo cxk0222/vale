ff87029742037092f64beb896661bc98696a7735
Yuriuh <642192014@qq.com>
2020-09-21 21:40:57 +0800

    Update Tabs demos

---
+++ src/components/Tabs1.demo.vue
@@ -0,0 +1,32 @@
+<demo>
+常规使用
+</demo>
+
+<template>
+  <div>
+    <Tabs v-model:selected="selected">
+      <Tab title="导航1">内容1</Tab>
+      <Tab title="导航2">内容2</Tab>
+    </Tabs>
+  </div>
+</template>
+
+<script lang="ts">
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+import {
+  ref
+} from 'vue'
+export default {
+  components: {
+    Tabs,
+    Tab
+  },
+  setup() {
+    const selected = ref('导航2')
+    return {
+      selected
+    }
+  }
+}
+</script>
--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,35 +1,21 @@
 <template>
   <div>
-    <div>
-      Tabs 示例
-    </div>
-    <h2>示例1</h2>
-    <Tabs v-model:selected="selected">
-      <Tab title="导航1">内容1</Tab>
-      <Tab title="导航222">内容2</Tab>
-    </Tabs>
+    <h1>Tabs 示例</h1>
+    <Demo :component="Tabs1Demo" />
   </div>
 </template>
 
 <script lang="ts">
-import Tabs from '../lib/Tabs.vue'
-import Tab from '../lib/Tab.vue'
-import { ref } from 'vue'
+import Demo from './Demo.vue'
+import Tabs1Demo from './Tabs1.demo.vue'
 export default {
   components: {
-    Tabs,
-    Tab,
+    Demo
   },
   setup() {
-    const selected = ref('导航222')
-
     return {
-      selected
+      Tabs1Demo
     }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
