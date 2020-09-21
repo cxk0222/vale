2aa8287dad01eb108b5d7a2d291214ee85ce2285
Yuriuh <642192014@qq.com>
2020-09-21 21:04:17 +0800

    Remove useless code

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -21,41 +21,10 @@ export default {
     Demo
   },
   setup() {
-    const bool = ref(false)
     return {
-      bool,
       Switch1Demo,
       Switch2Demo,
     }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-$border-color: #d9d9d9;
-.demo {
-  border: 1px solid $border-color;
-  margin: 16px 0 32px;
-  >h2 {
-    font-size: 20px;
-    padding: 8px 16px;
-    border-bottom: 1px solid $border-color;
-  }
-  &-component {
-    padding: 16px;
-  }
-  &-actions {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-  }
-  &-code {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-    >pre {
-      line-height: 1.1;
-      font-family: Consolas, 'Courier New', Courier, monospace;
-      margin: 0;
-    }
-  }
-}
-</style>
