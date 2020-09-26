43db73559cbad5038e97ffd1bd60c6d921f4ce02
Yuriuh <642192014@qq.com>
2020-09-26 17:28:22 +0800

    Add lib/index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,5 @@
+export { default as Swith } from './Switch.vue'
+export { default as Button } from './Button.vue'
+export { default as Tabs } from './Tabs.vue'
+export { default as Tab } from './Tab.vue'
+export { openDialog } from './openDialog'
