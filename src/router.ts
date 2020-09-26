1d1c15e40fbfb6a66635bf223e918832b5e2d079
Yuriuh <642192014@qq.com>
2020-09-26 16:09:14 +0800

    Resolve md file not import bug

--- src/router.ts
+++ src/router.ts
@@ -6,15 +6,19 @@ import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
+import intro from './markdown/intro.md'
+import getStarted from './markdown/get-started.md'
+import install from './markdown/install.md'
 
 import { h } from 'vue'
 import Markdown from './components/Markdown.vue'
 
 const history = createWebHashHistory()
-const md = filename => {
+
+const md = string => {
   return h(Markdown, {
-    path: `../markdown/${filename}.md`,
-    key: filename,
+    content: string,
+    key: string.slice(0, 20),
   })
 }
 
@@ -28,9 +32,9 @@ export const router = createRouter({
       component: Doc,
       children: [
         { path: '', redirect: '/doc/intro' },
-        { path: 'intro', component: md('intro') },
-        { path: 'get-started', component: md('get-started') },
-        { path: 'install', component: md('install') },
+        { path: 'intro', component: md(intro) },
+        { path: 'get-started', component: md(getStarted) },
+        { path: 'install', component: md(install) },
         { path: 'switch', component: SwitchDemo },
         { path: 'button', component: ButtonDemo },
         { path: 'dialog', component: DialogDemo },
